71a07767744839441ebac8563b2a0d55aca5466e
Falcon <36949730+huseynshahinduran@users.noreply.github.com>
2022-10-13 16:25:10 +0100

    Add files via upload

--- bankATM/bankATM/Program.cs
+++ bankATM/bankATM/Program.cs
@@ -8,54 +8,107 @@ namespace bankATM
 
         static void inputDetail(ref string password)
         {
-            Console.WriteLine("Please enter password:");
+            Console.WriteLine("Enter Your 4 Digit Pin:");
             password = Console.ReadLine();
         }
 
-        static void validateDetail(string password, int select, double balance)
+        static void validateDetail(string password, int select, int balance, int amount, int lastBalance)
         {
-            if (password == "pass")
+            if (password == "5842")
             {
-                menu(select, balance);
+                menu(select, balance, amount, lastBalance);
                 ok = true;
+                Console.Clear();
             }
             else
             {
-                Console.WriteLine("Wrong Password");
+                Console.WriteLine("Wrong Password\n\n");
             }
         }
 
-        static void menu(int select, double balance)
+        static void menu(int select, int balance, int amount, int lastBalance)
         {
+            Console.Clear();
+
             Console.WriteLine("1. Withdraw");
             Console.WriteLine("2. Deposite");
-            Console.WriteLine("3. Check Balance");
-            Console.WriteLine("4. Log out");
+            Console.WriteLine("3. Current Balance");
+            Console.WriteLine("4. Cancle");
             select = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
 
             switch (select)
             {
                 case 1:
-                    Console.WriteLine("*****WITHDRAW*****");
+                    Console.WriteLine("*****WITHDRAW*****\n");
+                    Console.Write("Enter the withdraw amount: ");
+                    amount = Convert.ToInt32(Console.ReadLine());
+                    lastBalance = balance - amount;
+                    Console.WriteLine($"Current balance: £{lastBalance}");
+
+                    Console.WriteLine("\n1. Return Back");
+                    Console.WriteLine("2. Cancle");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    if (select == 1)
+                    {
+                        menu(select, balance, amount, lastBalance);
+                    }
+                    else if (select == 2)
+                    {
+                        break;
+                    }
                     break;
                 case 2:
-                    Console.WriteLine("*****DEPOSIT*****");
+                    Console.WriteLine("*****DEPOSIT*****\n");
+                    Console.Write("Enter the deposit amount: ");
+                    amount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Your amount has benn deposited successfully");
+                    lastBalance = balance + amount;
+                    Console.WriteLine($"Current balance: £{lastBalance}");
+
+                    Console.WriteLine("\n1. Return Back");
+                    Console.WriteLine("2. Cancle");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    if (select == 1)
+                    {
+                        menu(select, balance, amount, lastBalance);
+                    }
+                    else if (select == 2)
+                    {
+                        break;
+                    }
                     break;
                 case 3:
-                    Console.WriteLine("*****CHECK BALANCE******");
+                    Console.WriteLine("*****CURRENT BALANCE******\n");
+                    Console.WriteLine($"Your current balance is £{balance}"); 
+
+                    Console.WriteLine("\n1. Return Back");
+                    Console.WriteLine("2. Cancle");
+                    select = Convert.ToInt32(Console.ReadLine());
+
+                    if (select == 1)
+                    {
+                        menu(select, balance, amount, lastBalance);
+                    }
+                    else if (select == 2)
+                    {
+                        break;
+                    }
                     break;
                 case 4:
-                    Console.WriteLine("*****LOG OUT*****");
                     break;
             }
         }
 
-        static void loop(string password, int select, double balance)
+        static void loop(string password, int select, int balance, int amount, int lastBalance)
         {
             for(int i=1; i<=3; i++)
             {
                 inputDetail(ref password);
-                validateDetail(password, select, balance);
+                validateDetail(password, select, balance, amount, lastBalance);
                  
                 if(ok == true)
                 {
@@ -72,9 +125,11 @@ namespace bankATM
         {
             string password = "";
             int select = 0;
-            double balance = 1000;
+            int balance = 1000;
+            int amount = 0;
+            int lastBalance = 0;
 
-            loop(password, select, balance);
+            loop(password, select, balance, amount, lastBalance);
         }
     }
 }
